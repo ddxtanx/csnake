ab4123d0249a2f3836dca857db0889b9c7e9e9c5
Garrett Credi (ddxtanx) <gcredi2@illinois.edu>
2022-11-01 17:38:03 -0500

    Create window

--- lib/screens/gtk_screen.c
+++ lib/screens/gtk_screen.c
@@ -2,11 +2,18 @@
 #include "gtk_screen.h"
 #include "../snake.h"
 #include <gtk/gtk.h>
+#include <stdio.h>
 
 GtkApplication* app;
 
-void activate_screen(GtkApplication* app){
+void activate_screen(GtkApplication* app, gpointer usr_data){
+    GtkWidget* window;
 
+    window = gtk_application_window_new(app);
+
+    gtk_window_set_title((GtkWindow*)window, "App!");
+    gtk_window_set_default_size((GtkWindow*)window, 400, 400);
+    gtk_widget_show(window);
 }
 
 extern void draw_state(state* s){
@@ -22,11 +29,12 @@ extern void close_screen(){
 }
 
 extern void get_max_x_y(int* y, int* x){
-
+    *y = 400;
+    *x = 400;
 }
 
 extern void init_screen(){
-    app = gtk_application_new("com.ddxtanx.snake", G_APPLICATION_DEFAULT_FLAGS);
+    app = gtk_application_new("com.ddxtanx.snake", G_APPLICATION_FLAGS_NONE);
 
     g_signal_connect(app, "activate", G_CALLBACK(activate_screen), NULL);
 
